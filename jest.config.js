ee6e120dd260f43bc6a4700488343eeb0cdb9da7
Ayoola Opere <opereayoola@gmail.com>
2026-09-25 06:21:49 -0500

    Added coverage settings to Jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     testMatch: ["**/test/**/*.test.ts"],
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!src/server.ts",
+        "!src/types/**/*.ts",
+    ],
 };
